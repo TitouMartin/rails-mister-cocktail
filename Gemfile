96ae7d83b2432aa98cca97064b3b0087c8dc206a
titouan martin <titouan.martin@free.fr>
2018-08-18 17:52:17 +0200

    some css done

--- Gemfile
+++ Gemfile
@@ -44,7 +44,8 @@ gem 'bootstrap-sass', '~> 3.3.7'
 gem 'font-awesome-sass', '~> 5.0.9'
 gem 'simple_form'
 gem 'awesome_print'
-
+gem 'json'
+gem 'rest-client'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-tequila_paf = Cocktail.create!(name: 'Tequila paf')
-tequila = Ingredient.create!(name: 'Tequila')
-salt = Ingredient.create!(name: 'Salt')
-lemon = Ingredient.create!(name: 'Lemon')
+# tequila_paf = Cocktail.create!(name: 'Tequila paf')
+# tequila = Ingredient.create!(name: 'Tequila')
+# salt = Ingredient.create!(name: 'Salt')
+# lemon = Ingredient.create!(name: 'Lemon')
 
-Dose.create!(ingredient_id: tequila.id, cocktail_id: tequila_paf.id, description: '3cl')
-Dose.create!(ingredient_id: salt.id, cocktail_id: tequila_paf.id, description: '1 pinch')
-Dose.create!(ingredient_id: lemon.id, cocktail_id: tequila_paf.id, description: '1 slice')
+# Dose.create!(ingredient_id: tequila.id, cocktail_id: tequila_paf.id, description: '3cl')
+# Dose.create!(ingredient_id: salt.id, cocktail_id: tequila_paf.id, description: '1 pinch')
+# Dose.create!(ingredient_id: lemon.id, cocktail_id: tequila_paf.id, description: '1 slice')
 
-mojito = Cocktail.create!(name: 'Mojito')
-mint = Ingredient.create!(name: 'Mint')
-rhum = Ingredient.create!(name: 'Rhum')
-sugar = Ingredient.create!(name: 'Sugar')
-sparkling_water = Ingredient.create!(name: 'Sparkling Water')
+# mojito = Cocktail.create!(name: 'Mojito')
+# mint = Ingredient.create!(name: 'Mint')
+# rhum = Ingredient.create!(name: 'Rhum')
+# sugar = Ingredient.create!(name: 'Sugar')
+# sparkling_water = Ingredient.create!(name: 'Sparkling Water')
 
 
-Dose.create!(ingredient_id: rhum.id, cocktail_id: mojito.id, description: '4cl')
-Dose.create!(ingredient_id: mint.id, cocktail_id: mojito.id, description: '10 leaves')
-Dose.create!(ingredient_id: lemon.id, cocktail_id: mojito.id, description: '3 slice')
-Dose.create!(ingredient_id: sugar.id, cocktail_id: mojito.id, description: '2 pinches')
-Dose.create!(ingredient_id: sparkling_water.id, cocktail_id: mojito.id, description: 'Your call !')
+# Dose.create!(ingredient_id: rhum.id, cocktail_id: mojito.id, description: '4cl')
+# Dose.create!(ingredient_id: mint.id, cocktail_id: mojito.id, description: '10 leaves')
+# Dose.create!(ingredient_id: lemon.id, cocktail_id: mojito.id, description: '3 slice')
+# Dose.create!(ingredient_id: sugar.id, cocktail_id: mojito.id, description: '2 pinches')
+# Dose.create!(ingredient_id: sparkling_water.id, cocktail_id: mojito.id, description: 'Your call !')
 
-cuba_libre = Cocktail.create!(name: 'Cuba Libre')
-coca_cola = Ingredient.create!(name: 'Coca Cola')
+# cuba_libre = Cocktail.create!(name: 'Cuba Libre')
+# coca_cola = Ingredient.create!(name: 'Coca Cola')
 
-Dose.create!(ingredient_id: rhum.id, cocktail_id: cuba_libre.id, description: '4cl')
-Dose.create!(ingredient_id: coca_cola.id, cocktail_id: cuba_libre.id, description: '15cl')
-Dose.create!(ingredient_id: lemon.id, cocktail_id: cuba_libre.id, description: '2 slice')
+# Dose.create!(ingredient_id: rhum.id, cocktail_id: cuba_libre.id, description: '4cl')
+# Dose.create!(ingredient_id: coca_cola.id, cocktail_id: cuba_libre.id, description: '15cl')
+# Dose.create!(ingredient_id: lemon.id, cocktail_id: cuba_libre.id, description: '2 slice')
+
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# response = RestClient.get(url)
+# new_ingredients = JSON.parse(response)
+# lets_go = new_ingredients.each { |e| e[1] }
+# lets_go.each { |f| f.each { |key, value| Ingredient.create!(name: "#{value}")} }
 
